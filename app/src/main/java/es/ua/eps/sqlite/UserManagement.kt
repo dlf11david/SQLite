5675da5cc44e20e8f2ad013cff2e8951f2b10881
davidlopez <dfl11@alu.ua.es>
2023-01-10 13:04:14 +0100

    spinner

--- app/src/main/java/es/ua/eps/sqlite/UserManagement.kt
+++ app/src/main/java/es/ua/eps/sqlite/UserManagement.kt
@@ -3,6 +3,9 @@ package es.ua.eps.sqlite
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import es.ua.eps.sqlite.databinding.ActivityUserManagementBinding
 
@@ -10,8 +13,30 @@ class UserManagement : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityUserManagementBinding.inflate(layoutInflater)
+        val sqlUsers = SQLiteCode(this)
+        val db = sqlUsers.readableDatabase
+        val cursor = db.rawQuery("SELECT nombre_usuario FROM usuarios", null)
         setContentView(binding.root)
+        var spinner_data = arrayOf<String>()
+        if (cursor.moveToFirst()) {
+            do {
+                spinner_data += cursor.getString(0).toString()
+            } while (cursor.moveToNext())
+        }
+        val adapter = ArrayAdapter<String>(
+            this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, spinner_data
+        )
+
         with(binding) {
+            spUsuario.adapter = adapter
+            spUsuario.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    Toast.makeText(this@UserManagement, "todo", Toast.LENGTH_SHORT).show()
+                }
+            }
             btnNuevo.setOnClickListener {
                 Intent(this@UserManagement,NewUser::class.java).also {
                     startActivity(it)
@@ -23,7 +48,7 @@ class UserManagement : AppCompatActivity() {
                 }
             }
             btnEliminar.setOnClickListener {
-                Toast.makeText(this@UserManagement,"Todo", Toast.LENGTH_SHORT).show()
+                sqlUsers.deleteData("")
             }
             btnLista.setOnClickListener {
                 Intent(this@UserManagement,ListUsers::class.java).also {
